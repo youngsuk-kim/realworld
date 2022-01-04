18b02ca3055702c019018eb4ed5748effaaa9974
youngsuk <bread@growingmom.com>
2022-01-05 01:07:49 +0900

    feat: getProfile 테스트 코드 추가

--- src/test/java/com/example/realworld/service/ProfileServiceImplTest.java
+++ src/test/java/com/example/realworld/service/ProfileServiceImplTest.java
@@ -11,14 +11,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.AdditionalAnswers;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ProfileServiceImplTest {
@@ -32,6 +36,34 @@ class ProfileServiceImplTest {
     @BeforeEach
     void initializeUserService() {
         this.profileService = new ProfileServiceImpl(userRepository, followRepository);
+    }
+
+    @Test
+    void getProfile(@Mock Principal principal) {
+        //given
+        String userName = "youngsuk1";
+
+        User user1 = new User(1L,userName,
+                "thayer2@gmail.com", "rladudtjr1!",
+                "bio", "imageurl", Authority.ROLE_USER);
+
+        User user2 = new User(2L,"youngsuk2",
+                "thayer2@gmail.com", "rladudtjr1!",
+                "bio", "imageurl", Authority.ROLE_USER, new ArrayList<>());
+
+        User user3 = new User(3L,"youngsuk3",
+                "thayer3@gmail.com", "rladudtjr1!",
+                "bio", "imageurl", Authority.ROLE_USER);
+
+        //when
+        when(principal.getName()).thenReturn("1");
+        when(userRepository.findByUserName(userName)).thenReturn(Optional.of(user1));
+        when(userRepository.findById(Long.valueOf(principal.getName()))).thenReturn(Optional.of(user2));
+
+        ProfileResponseDto response = profileService.getProfile(userName, principal);
+
+        //then
+        assertEquals(user1.getUserName(), response.getUserName());
     }
 
     @Test
@@ -58,6 +90,33 @@ class ProfileServiceImplTest {
 
         //then
         assertEquals(follow.getFollower().getUserName(), response.getUserName());
+    }
+
+    @Test
+    void unfollow(@Mock Principal principal) {
+        //given
+        String userName = "youngsuk1";
+
+        User user1 = new User(1L,userName,
+                "thayer2@gmail.com", "rladudtjr1!",
+                "bio", "imageurl", Authority.ROLE_USER);
+
+        User user2 = new User(2L,"youngsuk2",
+                "thayer2@gmail.com", "rladudtjr1!",
+                "bio", "imageurl", Authority.ROLE_USER);
+
+        Follow follow = new Follow(1L, user1, user2);
+
+        //when
+        when(principal.getName()).thenReturn("1");
+        when(userRepository.findByUserName(userName)).thenReturn(Optional.of(user1));
+        when(userRepository.findById(Long.valueOf(principal.getName()))).thenReturn(Optional.of(user2));
+        when(followRepository.findByUserAndFollower(user1, user2)).thenReturn(Optional.of(follow));
+        profileService.unfollow(userName, principal);
+
+        //then
+        verify(followRepository, times(1)).delete(follow);
+
     }
 
 }
